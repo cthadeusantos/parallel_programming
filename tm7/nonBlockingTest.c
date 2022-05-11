fe0dff152943977e45edd705e4ad7dd013eefe85
Carlos Thadeu <cthadeusantos@gmail.com>
2022-05-11 14:33:09 -0300

    troca de messagens - atividade 7, mpiSend/mpiRecv example

--- tm7/nonBlockingTest.c
+++ tm7/nonBlockingTest.c
@@ -10,10 +10,18 @@ int argc;
 char **argv;
 {   
     int rank,  size;
+    int buffer[buffer_count], recv_buffer[buffer_count];
 
     MPI_Request request;
     MPI_Status  status;
     int request_finished = 0;
+    int ready;
+
+
+    MPI_Init(&argc,&argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
 
     // Initialising buffer :
     for (int i=0; i < buffer_count; ++i)
@@ -28,7 +36,7 @@ char **argv;
 
       // 1- Initialise the non-blocking send to process 1
       // [...]
-            MPI_Isend(&buffer, count, MPI_INT, destination, tag, MPI_COMM_WORLD, &request); //non blocking send to destination process
+      MPI_Isend(&buffer, buffer_count, MPI_INT, 1, 321, MPI_COMM_WORLD, &request); //non blocking send to destination process
 
       double time_left = 6000.0;
       while (time_left > 0.0) {
@@ -37,12 +45,19 @@ char **argv;
         // 2- Test if the request is finished (only if not already finished)
         // [...]
         
+        MPI_Test(&request, &ready, MPI_STATUS_IGNORE);
+
         // 1ms left to work
         time_left -= 1000.0;
       }
 
       // 3- If the request is not yet complete, wait here. 
       // [...]
+      if (ready){
+        printf("enviado!");
+      } else {
+        MPI_Wait(&request, MPI_STATUS_IGNORE);
+      }
 
       // Modifying the buffer for second step
       for (int i=0; i < buffer_count; ++i)
@@ -50,19 +65,26 @@ char **argv;
 
       // 4- Prepare another request for process 1 with a different tag
       // [...]
-      
+      MPI_Isend(&buffer, buffer_count, MPI_INT, 1, 123, MPI_COMM_WORLD, &request); //non blocking send to destination process
+
       time_left = 3000.0;
       while (time_left > 0.0) {
         usleep(1000); // We work for 1ms
 
         // 5- Test if the request is finished (only if not already finished)
         // [...]
+        MPI_Test(&request, &ready, MPI_STATUS_IGNORE);
 
         // 1ms left to work
         time_left -= 1000.0;
       }
       // 6- Wait for it to finish
       // [...]
+      if (ready){
+        printf("enviado!");
+      } else {
+        MPI_Wait(&request, MPI_STATUS_IGNORE);
+      }
       
     }
     else {
@@ -71,22 +93,34 @@ char **argv;
 
       // 7- Initialise the non-blocking receive from process 0
       // [...]
+      MPI_Irecv(recv_buffer, buffer_count, MPI_INT, 0, 321, MPI_COMM_WORLD,&request);
 
       // 8- Wait here for the request to be completed
       // [...]
+      MPI_Wait(&request, MPI_STATUS_IGNORE);
 
-      print_buffer();
+      // print_buffer(recv_buffer);
+      printf("\n");
+      for (int i=0; i <buffer_count; i++){
+        printf("%d ", recv_buffer[i]);
+      }
       
       // Work for 3 seconds
       sleep(3);
 
       // 9- Initialise another non-blocking receive
       // [...]
+      MPI_Irecv(recv_buffer, buffer_count, MPI_INT, 0, 123, MPI_COMM_WORLD,&request);
       
       // 10- Wait for it to be completed
       // [...]
+      MPI_Wait(&request, MPI_STATUS_IGNORE);
 
-      print_buffer();
+      // print_buffer(buffer);
+      printf("\n");
+      for (int i=0; i <buffer_count; i++){
+        printf("%d ", recv_buffer[i]);
+      }
     }
     
 
@@ -101,6 +135,15 @@ char **argv;
     if (rank == 0)
       printf( "Total time for non-blocking scenario : %f seconds\n", final_time );
 
-  [....]
+  //[....]
+  MPI_Finalize();
 
 } // END OF MAIN
+
+
+// void print_buffer(int *buffer){
+//   int size = sizeof(&buffer)/sizeof(int);
+//   for (int i=0; i < size; i++){
+//         printf("%d ", buffer[i]);
+//   }
+// }
